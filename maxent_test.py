09a84c2a1d3d65d811b37a7219b89b70d92001f9
George <gpanterov@g-computer.(none)>
2014-07-19 02:05:28 -0400

    COmpleted nlls testing and max ent (ME)

---
+++ maxent_test.py
@@ -0,0 +1,210 @@
+import numpy as np
+from scipy.optimize import leastsq, minimize
+import pandas as pd
+import statsmodels.api as sm
+
+def nl_fform(params, rel_vars):
+	dependent_var =  params[0] * rel_vars[:, 0] + \
+					np.exp( params[1] * rel_vars[:, 1]) * \
+					(params[2] * rel_vars[:, 2]) 
+	return dependent_var
+
+def nl_fform2(params, rel_vars):
+	dependent_var =  params[0] * rel_vars[:, 0] * \
+					np.exp( params[1] * rel_vars[:, 1]) * \
+					(params[2] * rel_vars[:, 2]) 
+	return dependent_var
+
+
+def nl_fform3(params, rel_vars):
+	dependent_var =  params[0] * rel_vars[:, 0] * \
+					np.exp( params[1] * rel_vars[:, 1]) * \
+					(params[2] * rel_vars[:, 2]) 
+	return dependent_var
+
+
+def linear_fform2(params, rel_vars):
+	dependent_var = params[0] * rel_vars[:, 0] + params[1] * rel_vars[:, 1] + \
+					params[2] * rel_vars[:, 2]
+	return dependent_var
+
+
+
+def obj_func(x0, psupport, esupport):
+	X0 = x0.reshape((T + K, len(psupport)))	
+	P0 = X0[0:K, :]
+	W0 = X0[K:, :]
+	Hp = np.sum(np.sum(P0 * np.log(P0), axis=1))
+	Hw = np.sum(np.sum(W0 * np.log(W0), axis=1))
+	return Hp + Hw
+
+def obj_func_me(x0, psupport, esupport):
+	P = x0.reshape((K, len(psupport)))
+	Hp = np.sum(np.sum(P0 * np.log(P0), axis=1))
+	return Hp
+
+
+def constraint_moments(x0, num_of_var, y, X, psupport, esupport):
+	params = x0.reshape((T + K, len(psupport)))	
+	P0 = params[0:K, :]
+	W0 = params[K:, :]
+
+	pz = P0 * psupport
+
+	X0 = X[:,0].reshape((T,1))
+	X1 = X[:,1].reshape((T,1))
+	X2 = X[:,2].reshape((T,1))
+
+	e = y - (np.sum(np.kron(X0, pz[0,:]), axis=1) + \
+			 np.sum(np.kron(X1, pz[1,:]), axis=1) + \
+		 	 np.sum(np.kron(X2, pz[2,:]), axis=1) + \
+		 	 np.sum(W0 * esupport, axis=1) )
+	return np.sum(X[:, num_of_var] * e)
+
+def constraint_moments_me(x0, num_of_var, y, X, psupport, esupport):
+	P = x0.reshape((K, len(psupport)))	
+
+	pz = P * psupport
+
+	X0 = X[:,0].reshape((T,1))
+	X1 = X[:,1].reshape((T,1))
+	X2 = X[:,2].reshape((T,1))
+
+	e = y - (np.sum(np.kron(X0, pz[0,:]), axis=1) + \
+			 np.sum(np.kron(X1, pz[1,:]), axis=1) + \
+		 	 np.sum(np.kron(X2, pz[2,:]), axis=1) )
+	return np.sum(X[:, num_of_var] * e)
+
+
+def constraint_moments_me2(x0, num_of_var, y, X, psupport, esupport, fform):
+	P = x0.reshape((K, len(psupport)))	
+
+	params = np.sum(P * psupport, axis=1)
+	e = y - fform(params, X)
+	return np.sum(X[:, num_of_var] * e)
+
+def constraint_prob(x0):
+	params = x0.reshape((T + K, len(psupport)))	
+	s=np.sum(params, axis=1)
+	pbool = (s < 1 + 1e-10) * (s > 1 - 1e-10)
+	return T + K - np.sum(pbool)
+
+
+def constraint_prob_me(x0):
+	params = x0.reshape(( K, len(psupport)))	
+	s=np.sum(params, axis=1)
+	pbool = (s < 1 + 1e-10) * (s > 1 - 1e-10)
+	return K - np.sum(pbool)
+
+
+def normalize(nd_array, axis=0):
+	min_vals = np.min(nd_array, axis=axis)
+	max_vals = np.max(nd_array, axis=axis)
+	normalized_array = (nd_array - min_vals) / (max_vals - min_vals)
+	return normalized_array
+
+#########################################
+# Initial Parameters for the simulation #
+#########################################
+#np.random.seed(12345)
+T = 200
+K = 3
+params = [1, 1.5, 2.5]
+fform = linear_fform2
+
+###############
+# Create Data #
+###############
+rel_vars = np.random.normal(size=(T, K))
+noise = np.random.normal( size=(T, ))
+y = fform(params, rel_vars) + noise
+
+
+##################
+# Estimate Model #
+##################
+esupport = np.array([-1e2, 0., 1e2])
+psupport = np.array([-1e2, 0., 1e2])
+
+P0 = np.ones(shape=[K, len(psupport)]) / 3.
+W0 = np.ones(shape=(T, len(esupport))) / 3.
+
+x0 = np.row_stack((P0,W0)).flatten()
+x0me = P0.flatten()
+
+z = psupport
+pz = P0 * z
+
+X0 = rel_vars[:,0].reshape((T,1))
+X1 = rel_vars[:,1].reshape((T,1))
+X2 = rel_vars[:,2].reshape((T,1))
+
+e = y - (np.sum(np.kron(X0, pz[0,:]), axis=1) + \
+		 np.sum(np.kron(X1, pz[1,:]), axis=1) + \
+		 np.sum(np.kron(X2, pz[2,:]), axis=1) + \
+		 np.sum(W0 * esupport, axis=1) )
+
+cons = ({'type': 'eq',
+		 'fun': constraint_moments,
+		 'args': (0, y, rel_vars, psupport, esupport)},
+		{'type': 'eq',
+		 'fun': constraint_moments,
+		 'args': (1, y, rel_vars, psupport, esupport)},
+		{'type': 'eq',
+		 'fun': constraint_moments,
+		 'args': (2, y, rel_vars, psupport, esupport)})
+
+cons = ({'type': 'eq',
+		 'fun': constraint_moments,
+		 'args': (0, y, rel_vars, psupport, esupport)},
+		{'type': 'eq',
+		 'fun': constraint_prob}, )
+
+cons_me = ({'type': 'eq',
+		 'fun': constraint_moments_me,
+		 'args': (0, y, rel_vars, psupport, esupport)},
+		{'type': 'eq',
+		 'fun': constraint_moments_me,
+		 'args': (1, y, rel_vars, psupport, esupport)},
+		{'type': 'eq',
+		 'fun': constraint_moments_me,
+		 'args': (2, y, rel_vars, psupport, esupport)},
+		{'type': 'eq',
+		 'fun': constraint_prob_me}, )
+
+cons_me2 = ({'type': 'eq',
+		 'fun': constraint_moments_me2,
+		 'args': (0, y, rel_vars, psupport, esupport, fform)},
+		{'type': 'eq',
+		 'fun': constraint_moments_me2,
+		 'args': (1, y, rel_vars, psupport, esupport, fform)},
+		{'type': 'eq',
+		 'fun': constraint_moments_me2,
+		 'args': (2, y, rel_vars, psupport, esupport, fform)},
+		{'type': 'eq',
+		 'fun': constraint_prob_me}, )
+
+#sol = minimize(obj_func, x0, args=(psupport, esupport), 
+#				method='SLSQP', constraints=cons)
+
+sol = minimize(obj_func_me, x0me, args=(psupport, esupport),
+				method='SLSQP', constraints=cons_me2)
+###########
+# Results #
+###########
+X = sol['x'].reshape((K, 3))
+P = X[0:K, :]
+print sol['message']
+
+params_estim = np.sum(P * psupport, axis=1)
+print "The true value of the parameters is: ", params
+print "The ME parameter estimates are: ", params_estim
+#############
+# Benchmark #
+#############
+
+#model = sm.OLS(y, rel_vars)
+#results = model.fit()
+#y_ols = results.predict(exog=new_data)
+#ols_mse = np.mean((new_y - y_ols)**2)
+#print "Out of sample OLS mse is: ", ols_mse
---
+++ nlls_test.py
@@ -0,0 +1,141 @@
+import numpy as np
+from scipy.optimize import leastsq, minimize
+import pandas as pd
+import statsmodels.api as sm
+
+def nl_fform(params, rel_vars):
+	dependent_var =  params[0] * rel_vars[:, 0] + \
+					np.exp( params[1] * rel_vars[:, 1]) * \
+					(params[2] * rel_vars[:, 2]) 
+	return dependent_var
+
+def nl_fform2(params, rel_vars):
+	dependent_var =  params[0] * rel_vars[:, 0] * \
+					np.exp( params[1] * rel_vars[:, 1]) * \
+					(params[2] * rel_vars[:, 2]) 
+	return dependent_var
+
+
+def nl_fform3(params, rel_vars):
+	dependent_var =  params[0] * rel_vars[:, 0] * \
+					np.exp( params[1] * rel_vars[:, 1]) * \
+					(params[2] * rel_vars[:, 2]) 
+	return dependent_var
+
+
+def linear_fform(params, rel_vars):
+	dependent_var = params[0] * rel_vars[:, 0] + params[1] * rel_vars[:, 1] + \
+					params[2] * rel_vars[:, 2] + params[3] * rel_vars[:, 3] + \
+					params[4] * rel_vars[:, 4]
+	return dependent_var
+
+def linear_fform2(params, rel_vars):
+	dependent_var = params[0] * rel_vars[:, 0] + params[1] * rel_vars[:, 1] + \
+					params[2] * rel_vars[:, 2]
+	return dependent_var
+
+def sine_fform(params, rel_vars):
+	dependent_var = params[0] * rel_vars[:, 0] * np.sin(params[1] * \
+					rel_vars[:, 1]) * \
+					np.exp(params[2] * rel_vars[:, 2])
+	return dependent_var
+
+
+def obj_func(params, y, fform, data):
+	e = y - fform(params, data)
+	mse = np.mean(e ** 2)
+	return mse
+def normalize(nd_array, axis=0):
+	min_vals = np.min(nd_array, axis=axis)
+	max_vals = np.max(nd_array, axis=axis)
+	normalized_array = (nd_array - min_vals) / (max_vals - min_vals)
+	return normalized_array
+
+#########################################
+# Initial Parameters for the simulation #
+#########################################
+#np.random.seed(12345)
+n = 100000
+k = 3
+params = [1, 1.5, 2.5]
+
+noise_mean = 0
+noise_var = 10
+data_mean = 5
+data_var = 1
+
+fform = linear_fform2
+
+###############
+# Create Data #
+###############
+rel_vars = np.random.normal(size=(n, k))
+noise = np.random.normal( size=(n, ))
+y = fform(params, rel_vars) + noise
+
+data = np.column_stack((y, rel_vars))
+df = pd.DataFrame(data)
+
+new_data = np.random.normal(data_mean, data_var, size=(n/3, k))
+new_noise = np.random.normal(10, 2, size=(n/3, ))
+new_y = fform(params, new_data) + new_noise 
+
+
+##################
+# Estimate Model #
+##################
+
+# Inital values for optimization
+x0 = np.ones(shape=(k,))
+#x0 = np.random.normal(size=(k,))
+sol = minimize(obj_func, x0, args=(y, fform, rel_vars), method='Nelder-Mead')
+
+############
+# Results #
+############
+print sol['message']
+print "\n"
+
+yfit = fform(sol['x'], new_data)
+actual_mse = np.mean((new_y - fform(params, new_data)) ** 2)
+estim_mse = np.mean((new_y - fform(sol['x'], new_data)) ** 2)
+actual_pct_dev = np.mean(np.abs(new_y / fform(params, new_data)))
+estim_pct_dev = np.mean(np.abs(new_y / fform(sol['x'], new_data)))
+
+print "Out of sample TRUE MSE is: ", actual_mse
+print "Out of sample ESTIMATED mse is: ", estim_mse
+print "\n"
+
+bias = np.mean(new_y - fform(sol['x'], new_data))
+print "The bias of the estimator is: ", bias
+print "\n"
+
+res = np.column_stack((params, sol['x']))
+print "Actual            Estimates"
+print res
+
+#############
+# Benchmark #
+#############
+
+model = sm.OLS(y, rel_vars)
+results = model.fit()
+y_ols = results.predict(exog=new_data)
+ols_mse = np.mean((new_y - y_ols)**2)
+print "Out of sample OLS mse is: ", ols_mse
+
+rel_vars_norm = normalize(rel_vars)
+y_norm = normalize(y)
+#new_data_norm = normalize(new_data)
+new_data_norm = (new_data - np.min(rel_vars, 0)) / (np.max(rel_vars, 0) -\
+				np.min(rel_vars, 0))
+
+
+model_norm = sm.OLS(y_norm, rel_vars_norm)
+results = model_norm.fit()
+y_norm_fit = results.predict(exog=new_data_norm)
+
+y_renorm_fit = np.min(y) + y_norm_fit * (np.max(y) - np.min(y))
+
+renorm_mse = np.mean((new_y - y_renorm_fit)**2)
+print "Re-normalize out of sample OLS mse is: ", renorm_mse
