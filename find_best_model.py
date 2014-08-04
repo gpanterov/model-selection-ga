135bb05751fb1cfd935f4d89be042f624f91f12e
George <gpanterov@g-computer.(none)>
2014-08-04 09:15:27 -0400

    added ubuntu server file

---
+++ find_best_model.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import ga_tools as ga
+import data_tools as Data
+import random
+import data_models as models
+import numpy as np
+import sys
+import boto
+import aws_tools as aws_tools
+
+
+#inFile = sys.argv[1]
+
+bucket_name = 'georgipanterov.data.patterns'
+key_name = 'data_file.csv'
+#aws_tools.create_bucket(bucket_name)
+#key = aws_tools.store_private_data(bucket_name, key_name, 
+#	inFile)
+#print "Uploaded data to S3"
+
+data_file_name = 's3_data_file.csv'
+key2 = aws_tools.download_file(bucket_name, key_name, data_file_name )
+print "Downloaded data from S3"
+
+data = pd.read_csv(data_file_name, header=None)
+y = data.ix[:, 0]
+train_data = data.ix[:, 1:]
+N, Kall = np.shape(train_data)
+train_data.columns = range(Kall)
+
+
+
+# Fit Functions
+def fit_func(chrom):
+	return 1. * np.sum(chrom) / len(chrom)
+
+def model_fit(chrom, y, train_data):
+	X = train_data.ix[:, list(chrom)]
+	adjR2 = models.gof_ols(y, X)
+	n = np.sum(chrom)
+	b = 0
+	c = 5e-4
+	penalty = b * n + c * (n ** 2)
+	return  (1 + adjR2) * np.exp(-penalty)
+
+fit_func2 = lambda chrom: model_fit(chrom, y, train_data)
+
+pop_size =300
+num_gens = 100
+
+# length of chromosome
+l = len(train_data.columns)
+
+# Create population
+pop = []
+for i in range(pop_size):
+	chrom = np.asarray(np.random.randint(0, 2, size=(l, )), dtype=bool)
+	chrom = ga.make_chrom_valid(chrom)
+	pop.append(tuple(chrom))
+pop = tuple(pop)
+
+all_best_fit = []
+
+for i in range(num_gens):
+	fit = ga.get_fitness(pop, fit_func2)
+	pop = ga.offspring_pop(pop, fit, 
+			mating_func = ga.mate_random_cross, mutate_prob=0.1)
+
+	indx = np.argsort(fit)
+	best = pop[indx[-1]]
+	best_vars = np.where(np.array(best) == True)
+
+	all_best_fit.append(fit[indx[-1]])
+	if len(set(all_best_fit[-30:])) == 1 and len(all_best_fit) > 30:
+		print "Keep getting same results so we break"
+		break
+
+
+	print "Fitness is: ", fit[indx[-1]], np.sum(best), \
+					" ## ", best_vars[0][0:15]
+
+##########
+# Output #
+##########
+results_file_name = 'results.txt'
+f = open(results_file_name, 'w')
+model_vars = [str(i) for i in best_vars]
+model_vars = '-'.join(model_vars)
+print model_vars
+f.write(model_vars)
+f.close()
+
+bucket_name = 'georgipanterov.data.patterns.results'
+key_name = 'model_results.txt'
+aws_tools.create_bucket(bucket_name)
+print "done with output bucket"
+key = aws_tools.store_private_data(bucket_name, key_name, 
+	results_file_name)
+
